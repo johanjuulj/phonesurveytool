3709e154595dece0de1f72c88c157f026a458d11
Johan <johanjuulj@gmail.com>
2023-02-28 15:41:10 +0100

    start on linting

--- Phoner/__init__.py
+++ Phoner/__init__.py
@@ -1,28 +1,22 @@
+#
+""" Module for setting up the app environment variables and registering the blueprints """
 import os
 from flask import Flask
 from dotenv import load_dotenv
 from pymongo import MongoClient
-
 from Phoner.routes_survey import survey
 from Phoner.routes_notification import notification
 from Phoner.chatbot.routes import chatbot
 from Phoner.routes import pages
 
 load_dotenv()
-
-
 def create_app():
     app = Flask(__name__)
-    
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-    
     client = MongoClient(os.environ.get("MONGO_URI"))
     app.db = client.Phoner
-
     app.register_blueprint(pages)
     app.register_blueprint(chatbot)
     app.register_blueprint(notification)
     app.register_blueprint(survey)
     return app
-
-
